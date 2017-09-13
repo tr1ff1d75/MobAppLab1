f6c8e49c030f6f7267430564763d102ab0757968
tr1ff1d75 <gulko.dima@gmail.com>
2017-09-13 10:18:10 +0300

    Initial commit

---
+++ app/src/main/java/by/bstu/fit/gulko/mobapplab1/MainActivity.java
@@ -0,0 +1,70 @@
+package by.bstu.fit.gulko.mobapplab1;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import by.bstu.fit.gulko.mobapplab1.gulko.TextFunction;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        for (int count = 0; count < 10; count++) {
+            Log.d("MainActivity", "Counter"+count);
+        }
+        displayText();
+    }
+
+    private void displayText() {
+        TextFunction ntf = new TextFunction();
+        TextView nv= (TextView) findViewById(R.id.NewText);
+        nv.setText(ntf.getValue());
+    }
+
+    //TODO add new Method
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity  in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+                return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ app/src/main/java/by/bstu/fit/gulko/mobapplab1/gulko/TextFunction.java
@@ -0,0 +1,12 @@
+package by.bstu.fit.gulko.mobapplab1.gulko;
+
+/**
+ * Created by gulko on 06.09.2017.
+ */
+
+public class TextFunction {
+    public String getValue() {
+        return "Hello from first project";
+    }
+
+}
